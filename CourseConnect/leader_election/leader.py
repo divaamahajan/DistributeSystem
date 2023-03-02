c12286b9c163bfe41a24c5545873776114fec163
Divya <dm.divya.mahajan@gmail.com>
2023-03-02 11:42:53 -0800

    leaderElection

--- CourseConnect/leader_election/leader.py
+++ CourseConnect/leader_election/leader.py
@@ -29,10 +29,13 @@ class RingProtocolLeaderElection:
     
     
     def get_public_ip(self):
-        url = 'https://api.ipify.org'
-        response = requests.get(url)
-        return response.text
-    # print(get_public_ip())
+        try: 
+            url = 'https://api.ipify.org'
+            response = requests.get(url)
+            return response.text
+        except Exception as e:
+            print(f"\n\nError to get public Ip : {e}\n")
+    
 
     async def initiate_election(self):
         async with websockets.connect(f"ws://{self.next_ip}") as websocket:
@@ -40,59 +43,74 @@ class RingProtocolLeaderElection:
             await websocket.send(json.dumps(msg))
             
     async def election(self, electing_ip):
-        if electing_ip == self.my_ip:
-            nodes = scll()
-            nodes.add_node(self.my_ip)
-            async with self.lock:
-                self.leader_ip = self.my_ip
-                elected_leader_msg = {self.leader_elected_key: self.my_ip, self.allnodes_key:nodes}
+        try:
+            if electing_ip == self.my_ip:
+                nodes = scll()
+                nodes.add_node(self.my_ip)
+                async with self.lock:
+                    self.leader_ip = self.my_ip
+                    elected_leader_msg = {self.leader_elected_key: self.my_ip, self.allnodes_key:nodes}
+                    async with websockets.connect(f"ws://{self.next_ip}") as websocket:
+                        await websocket.send(json.dumps(elected_leader_msg))
+            else:
+                if electing_ip > self.my_ip:
+                    election_msg = {self.election_key: electing_ip}
+                elif electing_ip is None or electing_ip < self.my_ip:
+                    election_msg = {self.election_key: self.my_ip}
                 async with websockets.connect(f"ws://{self.next_ip}") as websocket:
-                    await websocket.send(json.dumps(elected_leader_msg))
-        else:
-            if electing_ip > self.my_ip:
-                election_msg = {self.election_key: electing_ip}
-            elif electing_ip is None or electing_ip < self.my_ip:
-                election_msg = {self.election_key: self.my_ip}
-            async with websockets.connect(f"ws://{self.next_ip}") as websocket:
-                await websocket.send(json.dumps(election_msg))
+                    await websocket.send(json.dumps(election_msg))
+        except Exception as e:
+            print(f"\n\nException while election : {e}\n")
         
     async def elected_leader(self, leader_ip, nodes:scll()):
-        nodes.add_node(self.my_ip)
-        async with self.lock:
-            if self.leader_ip == self.my_ip:
-                self.perform_leader_job(nodes)
-                return
-            self.leader_ip = leader_ip
-        elected_leader_msg =  {self.leader_elected_key: self.my_ip, self.allnodes_key:nodes}
-        async with websockets.connect(f"ws://{self.next_ip}") as websocket:
-            await websocket.send(json.dumps(elected_leader_msg))
+        try:
+            nodes.add_node(self.my_ip)
+            async with self.lock:
+                if self.leader_ip == self.my_ip:
+                    self.perform_leader_job(nodes)
+                    return
+                self.leader_ip = leader_ip
+            elected_leader_msg =  {self.leader_elected_key: self.my_ip, self.allnodes_key:nodes}
+            async with websockets.connect(f"ws://{self.next_ip}") as websocket:
+                await websocket.send(json.dumps(elected_leader_msg))
+        except Exception as e:
+            print(f"\n\nException while updating elected leader: {e}\n")
+        
 
     async def perform_leader_job(self, nodes:scll()):
-        #updates nodes list
-        self.ring_nodes = nodes        
-        if self.ring_nodes.head is None:
-            return
-        curr = self.ring_nodes.head
-        while True:
-            curr_ip = curr.data
-            next_ip = curr.next.data
-            update_successor_msg =  {self.successor_key: next_ip}
-            async with websockets.connect(f"ws://{curr_ip}") as websocket:
-                await websocket.send(json.dumps(update_successor_msg))
-            curr = curr.next
-            if curr == self.head:
-                break
+        try:
+            #updates nodes list
+            self.ring_nodes = nodes        
+            if self.ring_nodes.head is None:
+                return
+            node = self.ring_nodes.head
+            while True:
+                curr_ip = node.data
+                next_ip = node.get_successor(curr_ip)
+                update_successor_msg =  {self.successor_key: next_ip}
+                async with websockets.connect(f"ws://{curr_ip}") as websocket:
+                    await websocket.send(json.dumps(update_successor_msg))
+                node = node.next
+                if node == self.ring_nodes.head:
+                    break
+        except Exception as e:
+            print(f"\n\nException performing leader's tasks: {e}\n")
+        
 
     async def update_successor(self, successor_ip):
         self.next_ip = successor_ip
     
     async def listen(self):
-        async with websockets.connect(f"ws://{self.my_ip}") as websocket:
-            async for msg in websocket:
-                msg = json.loads(msg)
-                if self.election_key in msg.keys():
-                    await self.election(msg[self.election_key])
-                elif self.leader_elected_key in msg.keys():
-                    await self.elected_leader(msg[self.leader_elected_key],msg[self.allnodes_key] )
-                elif self.successor_key in msg.keys():
-                    await self.update_successor(msg[self.successor_key])
+        try:
+            async with websockets.connect(f"ws://{self.my_ip}") as websocket:
+                async for msg in websocket:
+                    msg = json.loads(msg)
+                    if self.election_key in msg.keys():
+                        await self.election(msg[self.election_key])
+                    elif self.leader_elected_key in msg.keys():
+                        await self.elected_leader(msg[self.leader_elected_key],msg[self.allnodes_key] )
+                    elif self.successor_key in msg.keys():
+                        await self.update_successor(msg[self.successor_key])
+        except Exception as e:
+            print(f"\n\nException while listening to message : {e}\n")
+        
